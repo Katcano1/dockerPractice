6f3ad68989993845842397f611a31f2c052089a6
Seth Cox <sethcox12345@gmail.com>
2024-05-21 15:54:11 -0400

    bruh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM python:3.12
+WORKDIR /opt/
 
-COPY . .
+COPY poetry.lock pyproject.toml ./
 
-CMD [ "python", "/QuickStart.py"]
+RUN apt update
+
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir poetry==1.6.*
+RUN poetry config virtualenvs.create false
+RUN poetry install -n
+
+
--- QuickStart.py
+++ QuickStart.py
@@ -1,3 +1,8 @@
+"""
+Instructions for running the Dockerized application
+
+"""
+
 
 class Queue:
     def __init__(self):
@@ -7,14 +12,16 @@ class Queue:
         self.queue.append(job)
 
     def dequeue(self):
+        # dequeues the element at the front unless the queue is empty
         if not self.isEmpty:
             return self.queue.pop(0)
         return
 
     def peek(self):
+        # returns the element at the front unless the queue is empty
         if not self.isEmpty:
             return self.queue[0]
-        return
+        return None
 
     def isEmpty(self):
         return len(self.queue) == 0
@@ -28,26 +35,36 @@ def sorting_key(e):
 
 
 def read_from_file(file_name, queue):
-    try:
-        file = open(file_name)
+    try:  # try statement in order to catch a bad file name
+        file = open(file_name)  # next 3 lines open, read, and then close the file
         lines = file.readlines()
         file.close()
 
+        value_errors = 0
+
+        # mechanism for splitting each line of the file into a list with 3 elements.
         temp_job_list = []
         for line in lines:
             temp_list = line.split(",")
+            # checks if the list has too many or too few elements
             if len(temp_list) != 3:
                 continue
-            try:
+            try:  # try statement to catch a priority value that isn't an integer
+                # converts the 'priority' element into an int for easy sorting
                 temp_list[2] = int(temp_list[2])
                 temp_job_list.append(temp_list)
             except ValueError:
+                value_errors += 1
                 continue
 
+        # sorts the list according to the priority values (lowest first)
         temp_job_list.sort(key=sorting_key)
 
         for job in temp_job_list:
-            queue.enqueue(job)
+            queue.enqueue(job)  # enqueues the jobs into a queue object
+
+        print("Successfully read from " + file_name)
+        print("Number of Value errors: " + str(value_errors))
 
         return queue
 
@@ -55,16 +72,13 @@ def read_from_file(file_name, queue):
         return queue
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_path = "jobs_small"
     q = Queue()
     q = read_from_file(file_path, q)
 
+    print("Printing queue to terminal...")
+    print("-----------------------------")
     for x in range(len(q.queue)):
         print("Running" + q.queue[x][1])
         q.dequeue()
-
-
-
-
-
