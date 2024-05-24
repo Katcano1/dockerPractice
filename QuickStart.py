b57dad804d6ad7c8ce4fac1bf685a489b0fc39ac
Seth Cox <sethcox12345@gmail.com>
2024-05-24 13:07:42 -0400

    made the sorting method more efficient and put it all in the enqueue method

--- QuickStart.py
+++ QuickStart.py
@@ -2,8 +2,8 @@
 Instructions for running the Dockerized application
 * Docker compose up -d
 - creates a docker container and runs it in the background
-* Docker attach <project name>
-- attaches the terminals input to the container
+* Docker attach <container name>
+- attaches the terminal's input to the container
 """
 
 import copy
@@ -15,7 +15,39 @@ class Queue:
         self.queue = []
 
     def enqueue(self, job):
-        self.queue.append(job)
+        if len(job) != 3:
+            print("Format is not correct.")
+            return
+
+            # checks if the priority is an integer
+        try:
+            job[2] = int(job[2])
+
+            # this is a way of checking if the uuid entered is of the correct format
+            # sample uuid: 550e8400-e29b-41d4-a716-446655440000
+            try:
+                uuid_test = UUID(job[0])
+            except ValueError:
+                print(
+                    "UUID is not acceptable. Must be of the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX."
+                )
+                return
+
+            temp_enqueue = copy.deepcopy(self)
+
+            for x in range(len(self.queue)):
+                if job[2] < self.queue[x][2]:
+                    temp_enqueue.queue.insert(x, job)
+                    print("task added successfully")
+                    return temp_enqueue
+
+            temp_enqueue.queue.append(job)
+
+            print("task added successfully")
+            return temp_enqueue
+        except ValueError:
+            print("priority entered is not acceptable. Must be an integer.")
+            return self
 
     def dequeue(self):
         # dequeues the element at the front unless the queue is empty
@@ -34,41 +66,19 @@ class Queue:
         return "{}, {}, {}".format(self.queue[0][0], self.queue[0][1], self.queue[0][2])
 
 
-def sorting_key(e):
-    return e[2]
-
-
 def read_from_file(file_name, queue):
     try:  # try statement in order to catch a bad file name
         file = open(file_name)  # next 3 lines open, read, and then close the file
         lines = file.readlines()
         file.close()
 
-        value_errors = 0
-
         # mechanism for splitting each line of the file into a list with 3 elements.
-        temp_job_list = []
         for line in lines:
             temp_list = line.split(",")
             # checks if the list has too many or too few elements
-            if len(temp_list) != 3:
-                continue
-            try:  # try statement to catch a priority value that isn't an integer
-                # converts the 'priority' element into an int for easy sorting
-                temp_list[2] = int(temp_list[2])
-                temp_job_list.append(temp_list)
-            except ValueError:
-                value_errors += 1
-                continue
-
-        # sorts the list according to the priority values (lowest first)
-        temp_job_list.sort(key=sorting_key)
-
-        for job in temp_job_list:
-            queue.enqueue(job)  # enqueues the list of jobs into a queue object
+            queue = queue.enqueue(temp_list)
 
         print("Successfully read from " + file_name)
-        print("Number of Value errors: " + str(value_errors))
 
         return queue
 
@@ -89,7 +99,7 @@ if __name__ == "__main__":
     while True:
 
         request = input(
-            "Type 'Enqueue' to add a job to the queue, 'Dequeue' to remove a job, 'Status' to view the current queue "
+            "Type 'Enqueue' to add a job to the queue, 'Dequeue' to run a job, 'Status' to view the current queue "
             "and queue size, or 'Quit' to exit the program.\n"
         )
 
@@ -100,41 +110,7 @@ if __name__ == "__main__":
 
             temp_list = new_job.split(",")
             # if the length of the list of elements entered is not 3, then there are either too many or too few elements
-            if len(temp_list) != 3:
-                print("Format is not correct.")
-                continue
-
-                # checks if the priority is an integer
-            try:
-                temp_list[2] = int(temp_list[2])
-
-                # this is a way of checking if the uuid entered is of the correct format
-                # sample uuid: 550e8400-e29b-41d4-a716-446655440000
-                try:
-                    uuid_test = UUID(temp_list[0])
-                except ValueError:
-                    print(
-                        "UUID is not acceptable. Must be of the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX."
-                    )
-                    continue
-
-                temp_jobs_list = []
-                for job in q.queue:
-                    temp_jobs_list.append(job)
-
-                temp_jobs_list.append(temp_list)
-                temp_jobs_list.sort(key=sorting_key)
-
-                temp_enqueue = Queue()
-
-                for job in temp_jobs_list:
-                    temp_enqueue.enqueue(job)
-
-                q = copy.deepcopy(temp_enqueue)
-                print("task added successfully")
-
-            except ValueError:
-                print("priority entered is not acceptable. Must be an integer.")
+            q = q.enqueue(temp_list)
 
         if request.lower() == "dequeue":
             # dequeues the front element and prints it out
